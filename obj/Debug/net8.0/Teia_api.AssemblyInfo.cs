d84638d4f091545b885861f316e09a8229c35e7c
Thiago Lima de Castro <tlcastro01@gmail.com>
2024-03-23 21:09:13 -0300

    ok

--- obj/Debug/net8.0/Teia_api.AssemblyInfo.cs
+++ obj/Debug/net8.0/Teia_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Teia_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe03766d2d089e2a6de8955eaada6cebc2fdf392")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96d74d60b3b0d0bc919f91a6bfba34ce15e0b88c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Teia_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Teia_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
